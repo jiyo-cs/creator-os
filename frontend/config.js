c7b8ef0751e4ad0cfe5e64f88b73b002f859c715
Mansouri Abdelali <mansouri.abdelali@univ-ouargla.dz>
2026-09-19 09:51:10 +0100

    Modify fetch to include credentials in requests
    Override the default fetch function to include credentials.

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,20 @@
 const API_URL =
     "https://creator-os-api-2bb9.onrender.com";
+
+
+const originalFetch =
+    window.fetch;
+
+
+window.fetch = function (
+    input,
+    init = {}
+) {
+
+    init.credentials = "include";
+
+    return originalFetch(
+        input,
+        init
+    );
+};
